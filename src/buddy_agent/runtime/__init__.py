5553b69f5cc0165ba62b16b6a8caaf5e6770a051
Prismtek <cody.sumpter@gmail.com>
2026-05-12 15:34:42 -0400

    Export runtime primitives

--- src/buddy_agent/runtime/__init__.py
+++ src/buddy_agent/runtime/__init__.py
@@ -1,1 +1,15 @@
 """Runtime shell for Buddy Agent."""
+
+from .engine import RuntimeEngine
+from .tools import ToolDefinition, ToolRegistry
+from .types import RuntimeMessage, RuntimeState, ToolCall, ToolResult
+
+__all__ = [
+    "RuntimeEngine",
+    "RuntimeMessage",
+    "RuntimeState",
+    "ToolCall",
+    "ToolDefinition",
+    "ToolRegistry",
+    "ToolResult",
+]
