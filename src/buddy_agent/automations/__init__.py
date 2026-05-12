2ebfaa8509067a9f457c77608641cbf9c5767b3c
Prismtek <cody.sumpter@gmail.com>
2026-05-12 18:49:53 -0400

    Export automation registry primitives

--- src/buddy_agent/automations/__init__.py
+++ src/buddy_agent/automations/__init__.py
@@ -1,1 +1,5 @@
 """Scheduled automation boundaries for Buddy Agent."""
+
+from .scheduler import AutomationJob, AutomationRegistry, AutomationRun
+
+__all__ = ["AutomationJob", "AutomationRegistry", "AutomationRun"]
