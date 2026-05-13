fbadf5f4633edf2fdb97510852918bfd13b4f8cb
Prismtek <cody.sumpter@gmail.com>
2026-05-13 06:28:14 -0400

    Export Buddy lifecycle helpers

--- src/buddy_agent/buddy/__init__.py
+++ src/buddy_agent/buddy/__init__.py
@@ -1,5 +1,6 @@
-"""Buddy domain models."""
+"""Buddy domain models and lifecycle helpers."""
 
+from .lifecycle import BuddyLifecycle, CareAction, CareResult
 from .models import BuddyArchetype, BuddyProfile
 
-__all__ = ["BuddyArchetype", "BuddyProfile"]
+__all__ = ["BuddyArchetype", "BuddyLifecycle", "BuddyProfile", "CareAction", "CareResult"]
