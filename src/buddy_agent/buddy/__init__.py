77ea180fed929e5439b42ab18c1e9ec5abef63bd
Prismtek <cody.sumpter@gmail.com>
2026-05-13 12:01:02 -0400

    Export Buddy appearance contracts

--- src/buddy_agent/buddy/__init__.py
+++ src/buddy_agent/buddy/__init__.py
@@ -1,6 +1,16 @@
-"""Buddy domain models and lifecycle helpers."""
+"""Buddy domain models, lifecycle helpers, and appearance contracts."""
 
+from .appearance import BuddyAppearanceTemplate, BuddyFrameSpec, default_buddy_template
 from .lifecycle import BuddyLifecycle, CareAction, CareResult
 from .models import BuddyArchetype, BuddyProfile
 
-__all__ = ["BuddyArchetype", "BuddyLifecycle", "BuddyProfile", "CareAction", "CareResult"]
+__all__ = [
+    "BuddyAppearanceTemplate",
+    "BuddyArchetype",
+    "BuddyFrameSpec",
+    "BuddyLifecycle",
+    "BuddyProfile",
+    "CareAction",
+    "CareResult",
+    "default_buddy_template",
+]
