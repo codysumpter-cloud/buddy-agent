8ff6b5074b62f4109b5cf6d0102cdc2f97396f16
Prismtek <cody.sumpter@gmail.com>
2026-05-13 07:32:46 -0400

    Export local scaffold adapters

--- src/buddy_agent/__init__.py
+++ src/buddy_agent/__init__.py
@@ -1,5 +1,18 @@
 """Buddy Agent package."""
 
+from .local_adapters import (
+    LocalBuddyBrainAdapter,
+    LocalKnowledgeVaultProvider,
+    LocalOmniBuddyAdapter,
+    LocalPrismtekAppBridge,
+)
 from .metadata import PROJECT_NAME, VERSION
 
-__all__ = ["PROJECT_NAME", "VERSION"]
+__all__ = [
+    "LocalBuddyBrainAdapter",
+    "LocalKnowledgeVaultProvider",
+    "LocalOmniBuddyAdapter",
+    "LocalPrismtekAppBridge",
+    "PROJECT_NAME",
+    "VERSION",
+]
