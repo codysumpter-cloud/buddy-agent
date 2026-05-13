c59c249d3d69eb1c94b3a03a38dc271442f1f379
Prismtek <cody.sumpter@gmail.com>
2026-05-13 06:29:06 -0400

    Export Omni config model

--- src/buddy_agent/omni/__init__.py
+++ src/buddy_agent/omni/__init__.py
@@ -1,1 +1,5 @@
 """Omni/local runtime bridge boundaries for Buddy Agent."""
+
+from .config import OmniConfig, OmniRouteMode, OmniVisionMode
+
+__all__ = ["OmniConfig", "OmniRouteMode", "OmniVisionMode"]
