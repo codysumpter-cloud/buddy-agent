5da970430a6e0800a41c4eb6860194b031deed79
Prismtek <cody.sumpter@gmail.com>
2026-05-13 15:12:09 -0400

    Export persistent memory index

--- src/buddy_agent/memory/__init__.py
+++ src/buddy_agent/memory/__init__.py
@@ -1,5 +1,6 @@
 """Memory and retrieval boundaries for Buddy Agent."""
 
 from .index import NoteIndex, NoteRecord
+from .persistent import DEFAULT_MEMORY_PATH, PersistentNoteIndex
 
-__all__ = ["NoteIndex", "NoteRecord"]
+__all__ = ["DEFAULT_MEMORY_PATH", "NoteIndex", "NoteRecord", "PersistentNoteIndex"]
