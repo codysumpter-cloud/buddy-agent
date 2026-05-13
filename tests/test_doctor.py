6c681f05bb49d1f81ca7e2c5b4af6bef6e3b632a
Prismtek <cody.sumpter@gmail.com>
2026-05-13 09:52:26 -0400

    Add doctor tests

---
+++ tests/test_doctor.py
@@ -0,0 +1,14 @@
+from buddy_agent.doctor import doctor_ok, run_doctor
+
+
+def test_run_doctor_returns_passing_checks():
+    checks = run_doctor()
+
+    assert doctor_ok(checks) is True
+    assert {check.name for check in checks} == {
+        "runtime",
+        "buddy-brain-adapter",
+        "omni-adapter",
+        "app-bridge",
+        "vault-provider",
+    }
