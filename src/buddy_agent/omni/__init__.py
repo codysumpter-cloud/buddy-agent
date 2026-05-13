89f60f177843b4843d2eedba3413a0aafb7ce6be
Prismtek <cody.sumpter@gmail.com>
2026-05-13 17:49:49 -0400

    Export callable Omni backend contract

--- src/buddy_agent/omni/__init__.py
+++ src/buddy_agent/omni/__init__.py
@@ -1,5 +1,12 @@
 """Omni/local runtime bridge boundaries for Buddy Agent."""
 
+from .backend import CallableTextBackend, TextRouteBackend
 from .config import OmniConfig, OmniRouteMode, OmniVisionMode
 
-__all__ = ["OmniConfig", "OmniRouteMode", "OmniVisionMode"]
+__all__ = [
+    "CallableTextBackend",
+    "OmniConfig",
+    "OmniRouteMode",
+    "OmniVisionMode",
+    "TextRouteBackend",
+]
