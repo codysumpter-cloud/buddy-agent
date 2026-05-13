ea9247cbf587be3742d8972f2f0d2beb7d8bbb34
Prismtek <cody.sumpter@gmail.com>
2026-05-13 12:07:33 -0400

    Add companion namespace exports

---
+++ src/buddy_agent/companion/__init__.py
@@ -0,0 +1,13 @@
+"""Companion shell contracts for floating Buddy and iBeMore surfaces."""
+
+from .contracts import CompanionCapability, CompanionEvent, CompanionState
+from .permissions import CompanionPermissionPolicy, PermissionDecision, PermissionRequest
+
+__all__ = [
+    "CompanionCapability",
+    "CompanionEvent",
+    "CompanionPermissionPolicy",
+    "CompanionState",
+    "PermissionDecision",
+    "PermissionRequest",
+]
