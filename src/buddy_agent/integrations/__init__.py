d80b80adbbbf2a245365a45e30ce9fabcc830fc1
Prismtek <cody.sumpter@gmail.com>
2026-05-13 20:03:16 -0400

    Add integration package for AgentCraft bridge

---
+++ src/buddy_agent/integrations/__init__.py
@@ -0,0 +1,10 @@
+"""Optional Buddy Agent integration bridges."""
+
+from .agentcraft import AgentCraftBridge, AgentCraftConfig, AgentCraftEmitResult, AgentCraftEvent
+
+__all__ = [
+    "AgentCraftBridge",
+    "AgentCraftConfig",
+    "AgentCraftEmitResult",
+    "AgentCraftEvent",
+]
