6a55b67d57b44c3db79b68fa4be8347b8d7788bb
Prismtek <cody.sumpter@gmail.com>
2026-05-13 21:10:01 -0400

    Export app bridge route

--- src/buddy_agent/app_bridge/__init__.py
+++ src/buddy_agent/app_bridge/__init__.py
@@ -1,16 +1,14 @@
 """Prismtek app bridge boundaries for Buddy Agent."""
 
-from .contracts import (
-    BUDDY_EVENT_NAMES,
-    BuddyEvent,
-    BuddyEventName,
-    TradePackageSummary,
-    normalize_buddy_event_name,
-)
+from .contracts import BUDDY_EVENT_NAMES, BuddyEvent, BuddyEventName, TradePackageSummary
+from .contracts import normalize_buddy_event_name
 from .ibe_more import IBeMoreAction, IBeMoreRequest, IBeMoreResponse, IBeMoreSurface
+from .runtime_route import AppChatRequest, AppChatResponse, route_app_chat
 
 __all__ = [
     "BUDDY_EVENT_NAMES",
+    "AppChatRequest",
+    "AppChatResponse",
     "BuddyEvent",
     "BuddyEventName",
     "IBeMoreAction",
@@ -19,4 +17,5 @@ __all__ = [
     "IBeMoreSurface",
     "TradePackageSummary",
     "normalize_buddy_event_name",
+    "route_app_chat",
 ]
