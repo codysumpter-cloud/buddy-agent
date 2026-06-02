be87cbfcae3c601a6e106f56f8a272b56308811d
Prismtek <cody.sumpter@gmail.com>
2026-06-02 14:48:30 -0400

    Export native OpenMythos and Symphony helpers

--- src/buddy_agent/integrations/__init__.py
+++ src/buddy_agent/integrations/__init__.py
@@ -13,7 +13,24 @@ from .contracts import (
     IntegrationTarget,
     parse_integration_id,
 )
+from .openmythos import (
+    BuddyMythosConfig,
+    get_variant_config,
+    torch_backend_lines,
+    torch_backend_status,
+    training_plan_lines,
+    variant_summary_lines,
+)
 from .runtime import BuddyIntegrationRuntime, IntegrationRunResult
+from .symphony import (
+    BuddyWorkflow,
+    BuddyWorkIssue,
+    BuddyWorkRunPlan,
+    build_work_run_plan,
+    create_local_workspace,
+    load_workflow,
+    parse_workflow_text,
+)
 from .targets import (
     get_integration_target,
     integration_summary_lines,
@@ -26,13 +43,26 @@ __all__ = [
     "AgentCraftEmitResult",
     "AgentCraftEvent",
     "BuddyIntegrationRuntime",
+    "BuddyMythosConfig",
+    "BuddyWorkflow",
+    "BuddyWorkIssue",
+    "BuddyWorkRunPlan",
     "IntegrationCapability",
     "IntegrationId",
     "IntegrationRunResult",
     "IntegrationStatus",
     "IntegrationTarget",
+    "build_work_run_plan",
+    "create_local_workspace",
     "get_integration_target",
+    "get_variant_config",
     "integration_summary_lines",
+    "load_workflow",
     "parse_integration_id",
+    "parse_workflow_text",
+    "torch_backend_lines",
+    "torch_backend_status",
+    "training_plan_lines",
     "validate_integration_targets",
+    "variant_summary_lines",
 ]
