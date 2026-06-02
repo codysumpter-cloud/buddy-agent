fda1c1e8267cefab6325d614d6c578ba976d7bd8
Prismtek <cody.sumpter@gmail.com>
2026-06-02 15:28:20 -0400

    feat: export Buddy action loop runtime

--- src/buddy_agent/runtime/__init__.py
+++ src/buddy_agent/runtime/__init__.py
@@ -1,10 +1,32 @@
 """Runtime shell for Buddy Agent."""
 
+from .action_loop import (
+    ACTION_SCHEMA_VERSION,
+    SESSION_SCHEMA_VERSION,
+    AgentProfile,
+    BuddyAction,
+    BuddyActionLoopRuntime,
+    BuddyAgentSession,
+    BuddyDelegation,
+    BuddyReceipt,
+    BuddyWorldState,
+    WorkerReport,
+    requires_human_approval,
+)
 from .engine import RuntimeEngine
 from .tools import ToolDefinition, ToolRegistry
 from .types import RuntimeMessage, RuntimeState, ToolCall, ToolResult
 
 __all__ = [
+    "ACTION_SCHEMA_VERSION",
+    "SESSION_SCHEMA_VERSION",
+    "AgentProfile",
+    "BuddyAction",
+    "BuddyActionLoopRuntime",
+    "BuddyAgentSession",
+    "BuddyDelegation",
+    "BuddyReceipt",
+    "BuddyWorldState",
     "RuntimeEngine",
     "RuntimeMessage",
     "RuntimeState",
@@ -12,4 +34,6 @@ __all__ = [
     "ToolDefinition",
     "ToolRegistry",
     "ToolResult",
+    "WorkerReport",
+    "requires_human_approval",
 ]
