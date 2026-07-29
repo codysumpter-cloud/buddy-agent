eb5099ec02e7a78251a1385a2241a50d4e68c513
Prismtek <cody.sumpter@gmail.com>
2026-07-29 08:56:09 -0400

    feat: add Trust Fabric package

---
+++ src/buddy_agent/trust_fabric/__init__.py
@@ -0,0 +1,5 @@
+"""Prismtek Trust Fabric: evidence normalization, policy, and verified receipts."""
+
+from .pipeline import evaluate_retrieval, finalize_run
+
+__all__ = ["evaluate_retrieval", "finalize_run"]
